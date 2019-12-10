eb777298235ee0009d6cc255d7a24a00cd8219e5
Marisa Canan-Zucker <mcananzucker@gmail.com>
2019-12-10 16:58:57 -0500

    nav bar fixed to top

--- src/Components/Session/NavBar.js
+++ src/Components/Session/NavBar.js
@@ -7,64 +7,46 @@ import ProgressBar from './ProgressBar'
 const NavBar = props => {
   // let {title, subtitle, user} = props.session.project
 
-  // console.log('NavBar props = ', props)
-
-  // console.log('NavBar props.session.project = ', props.project.title)
   return (
-    <Sticky>
-      <Header block fluid>
 
-        {/* <Menu color='black' fixed='top' inverted borderless size='large'>
 
-          <Menu.Item position='left'>
-          <Statistic inverted size='mini'>
+  <Segment attached="bottom" secondary>
+
+    <Grid verticalAlign='middle'>
+
+      <Grid.Column width={4}>
+        {/* left - user email */}
+        <Statistic size='mini' color='teal'>
           <Statistic.Label>Hosted By</Statistic.Label>
           <Statistic.Value text>
-          <Icon name='user' circular />
-          {props.host ? props.host : null}
+            <Icon name='user' circular />
+            {props.host ? props.host : null}
           </Statistic.Value>
-          </Statistic>
-        </Menu.Item> */}
+        </Statistic>
+      </Grid.Column>
 
-        {/* <Menu.Item position='middle'>
-          <Header inverted as='h3'>{props.title ? props.title : null}</Header>
-          <Header inverted as='h4'>{props.subtitle ? props.subtitle : null}</Header>
+      {/*  center - title */}
+      <Grid.Column
+        width={8}
+        textAlign='center'
+      >
+        <Header as="h2"
+          content={props.title ? props.title : null}
+          subheader={props.subtitle ? props.subtitle : null}
+        />
 
-        </Menu.Item> */}
+        {/* <ProgressBar /> */}
+      </Grid.Column>
 
-        {/* <Menu.Item position='right'> */}
-        {/* <Button>Submit Answers</Button> */}
+      <Grid.Column width={4}>
+        <Button basic disabled>Check Answers</Button>
+      </Grid.Column>
 
-        {/* </Menu.Item> */}
 
-        {/* </Menu> */}
+    </Grid>
 
-        <Grid verticalAlign='middle'>
-          <Grid.Column width={4}>
-            {/* left - user email */}
-            <Statistic size='mini' color='teal'>
-              <Statistic.Label>Hosted By</Statistic.Label>
-              <Statistic.Value text>
-                <Icon name='user' circular />
-                {props.host ? props.host : null}
-              </Statistic.Value>
-            </Statistic>
-          </Grid.Column>
 
-          {/*  center - title */}
-          <Grid.Column width={8} as={'h1'} textAlign='center'>
-            {props.title ? props.title : null}
-            <Header as='h4'>{props.subtitle ? props.subtitle : null}</Header>
-          </Grid.Column>
-
-          <Grid.Column width={4}>
-            <Button basic>Check Answers</Button>
-          </Grid.Column>
-
-        </Grid>
-
-      </Header>
-    </Sticky>
+  </Segment>
 
 
 
