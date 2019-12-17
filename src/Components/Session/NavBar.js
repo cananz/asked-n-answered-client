d9489f2c044d75dcdf3b9ef77d38a5bfb382045b
Marisa Canan-Zucker <mcananzucker@gmail.com>
2019-12-17 16:29:24 -0500

    move title and createdBy to own components

--- src/Components/Session/NavBar.js
+++ src/Components/Session/NavBar.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import {Statistic, Segment, Header, Grid, Icon, Menu, Button, Container, Sticky} from 'semantic-ui-react'
+import {Statistic, Segment, Grid, Loader} from 'semantic-ui-react'
 import ProgressBar from './ProgressBar'
-
+import CreatedBy from './CreatedBy'
+import Title from './Title'
 
 const NavBar = props => {
   // let {title, subtitle, user} = props.session.project
@@ -13,40 +14,34 @@ const NavBar = props => {
   <Segment attached="top" secondary>
 
 
-    <Grid verticalAlign='middle'>
+    <Grid columns='equal' verticalAlign='middle'>
 
-      <Grid.Column width={4}>
-        {/* left - user email */}
-        <Statistic size='mini' color='teal'>
-          <Statistic.Label>Hosted By</Statistic.Label>
-          <Statistic.Value text>
-            <Icon name='user' circular />
-            {props.host ? props.host : null}
-          </Statistic.Value>
-        </Statistic>
-
+      <Grid.Column>
+        <Statistic
+          color="teal"
+          size="mini"
+          value={props.pin}
+          label="pin"
+          floated="left"
+        />
       </Grid.Column>
 
       {/*  center - title */}
-      <Grid.Column
-        width={8}
-        textAlign='center'
-      >
-        <Header as="h2"
-          content={props.title ? props.title : null}
-          subheader={props.subtitle ? props.subtitle : null}
-        />
+      <Grid.Column width={8} textAlign='center'>
+        {props.title ? <Title /> : <Loader active />}
 
-        {/* <ProgressBar /> */}
       </Grid.Column>
 
-      <Grid.Column width={4}>
-        <Button basic disabled>Check Answers</Button>
+
+
+      <Grid.Column>
+        {/* <Loader active /> */}
+        <CreatedBy />
       </Grid.Column>
 
 
     </Grid>
-    {/* <ProgressBar /> */}
+
 
   </Segment>
 
